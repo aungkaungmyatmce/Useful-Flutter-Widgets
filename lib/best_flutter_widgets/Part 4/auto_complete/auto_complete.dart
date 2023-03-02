63e9105c230e1c728fd1e71a68684cbd7bb90853
aungkaungmyat <aungkaungmyat.mce@gmail.com>
2023-03-02 11:36:45 +0630

    updating autocomplete

--- lib/best_flutter_widgets/Part 4/auto_complete/auto_complete.dart
+++ lib/best_flutter_widgets/Part 4/auto_complete/auto_complete.dart
@@ -18,6 +18,40 @@ class AutoCompleteWidget extends StatelessWidget {
       onSelected: (String item) {
         print('$item is selected');
       },
+      fieldViewBuilder: (BuildContext context,
+          TextEditingController textEditingController,
+          FocusNode focusNode,
+          VoidCallback onFieldSubmitted) {
+        return TextField(
+          decoration: InputDecoration(border: OutlineInputBorder()),
+          controller: textEditingController,
+          focusNode: focusNode,
+          onSubmitted: (String value) {},
+        );
+      },
+      optionsViewBuilder: (BuildContext context,
+          void Function(String) onSelected, Iterable<String> options) {
+        return Material(
+            child: SizedBox(
+                height: 200,
+                child: SingleChildScrollView(
+                    child: Column(
+                  children: options.map((opt) {
+                    return InkWell(
+                        onTap: () {
+                          onSelected(opt);
+                        },
+                        child: Container(
+                            padding: EdgeInsets.only(right: 60),
+                            child: Card(
+                                child: Container(
+                              width: double.infinity,
+                              padding: EdgeInsets.all(10),
+                              child: Text(opt),
+                            ))));
+                  }).toList(),
+                ))));
+      },
     );
   }
 }
